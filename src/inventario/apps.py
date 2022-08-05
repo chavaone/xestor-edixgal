f3980675c1fd89ffe7d9518daf6039591e7f3fd3
Marcos Chavarría Teijeiro <chavarria1991@gmail.com>
2022-08-05 13:21:08 +0200

    [Backups] Programar creación de backups no inicio do programa.

--- src/inventario/apps.py
+++ src/inventario/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
-
+from django.core import management
 
 class InventarioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'inventario'
+
+    def ready(self):
+        management.call_command('dbbackup')
+        management.call_command('mediabackup')
+        return True
